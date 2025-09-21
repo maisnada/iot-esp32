ac782ca0254bb3706601739739ed4c0b8dea0336
marcelo <maisnada@gmail.com>
2025-09-21 18:16:07 -0300

    test time and display

--- pomodoro/pomodoro.ino
+++ pomodoro/pomodoro.ino
@@ -2,7 +2,7 @@
 #include <LiquidCrystal_I2C.h> 
 
 #define colunas 16 
-#define linhas  2 
+#define linhas 2 
 #define endereco  0x27 
 
 LiquidCrystal_I2C lcd(endereco,colunas,linhas); 
@@ -11,14 +11,23 @@ int currentState;
 
 int lastState = LOW; 
 
+bool work = false;
+int timeToWork = 1800;
+int timeToBreak = 300;
+int timeToBreakLong = 600;
+int cycle = 0;
+int count = 0;
+
+char buffer[17]; 
+
 void bip(){
 
   tone(3, 1200); 
-  delay(400);  
-  noTone(3);
-  delay(100);
-  tone(3, 1200); 
   delay(200);  
+  noTone(3);
+  delay(150);
+  tone(3, 1200); 
+  delay(150);  
   noTone(3);
 }
 
@@ -36,16 +45,36 @@ void setup()
 
 void loop() 
 {
-  /*delay(3000);  
-  lcd.clear(); */
-
+ 
   lcd.setCursor(0,0); 
   
   currentState = digitalRead(2);
 
+  if(work){
+   
+    sprintf(buffer, "Work %d seconds ", count); 
+
+    lcd.print(buffer);
+
+    //lcd.setCursor(0,1); 
+
+    //lcd.print(count); 
+
+    count++;
+
+    delay(1000);
+
+    buffer[0] = '\0';
+
+    return;
+
+  }
+
   if(lastState == LOW && currentState == HIGH){
     
-    lcd.print("OK"); 
+    lcd.print("Work"); 
+
+    work = true;
 
     bip();
 
@@ -53,13 +82,11 @@ void loop()
 
       lcd.print("NO"); 
     }    
-
   
   lastState = currentState;
 
   delay(500);  
   lcd.clear(); 
-
  
  /*
  for (int positionCounter = 0; positionCounter < 13; positionCounter++) {
