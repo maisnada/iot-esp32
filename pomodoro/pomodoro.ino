e320d81a2657d4ade79b34071e04e95bcec8502f
Marcelo <maisnada@gmail.com>
2025-09-23 09:26:33 -0300

    pomodoro tests

--- pomodoro/pomodoro.ino
+++ pomodoro/pomodoro.ino
@@ -12,13 +12,16 @@ int currentState;
 int lastState = LOW; 
 
 bool work = false;
-int timeToWork = 1800;
-int timeToBreak = 300;
+bool breakTime = false;
+int timeToBreak = 200;
+//int timeToWork = 1800;
+int timeToWork = 200;
 int timeToBreakLong = 600;
 int cycle = 0;
 int count = 0;
 
 char buffer[17]; 
+char bufferAux[17];
 
 void bip(){
 
@@ -46,25 +49,78 @@ void setup()
 void loop() 
 {
  
-  lcd.setCursor(0,0); 
+  lcd.clear(); 
   
   currentState = digitalRead(2);
 
   if(work){
-   
-    sprintf(buffer, "Work %d sec.", count); 
+  
+    buffer[0] = '\0';
+    bufferAux[0] = '\0';
+    
+    lcd.setCursor(0,0);
+
+    sprintf(bufferAux, "Work | Cycle: %d", cycle);
+
+    lcd.print(bufferAux);
+
+    lcd.setCursor(0,1);
+     
+    sprintf(buffer, "%d seconds", count); 
 
     lcd.print(buffer);
 
-    //lcd.setCursor(0,1); 
+    count++;
 
-    //lcd.print(count); 
+    delay(200);
+    
+    if(count > timeToWork){
+
+      count = 0;
+      work = false;
+      breakTime = true;
+
+      bip();
+    }
+
+    return;
+
+  }
+
+  if(breakTime){
+
+    buffer[0] = '\0';
+    bufferAux[0] = '\0';
+    
+    if(cycle == 3){
+
+    }
+
+    lcd.setCursor(0,0);
+
+    sprintf(bufferAux, "Break | Cycle: %d", cycle);
+
+    lcd.print(bufferAux);
+
+    lcd.setCursor(0,1);
+     
+    sprintf(buffer, "%d seconds", count); 
+
+    lcd.print(buffer);
 
     count++;
 
-    delay(1000);
+    delay(200);
 
-    buffer[0] = '\0';
+    if(count > timeToBreak){
+
+      count = 0;
+      cycle++;
+      work = true;
+      breakTime = false;
+
+      bip();
+    }
 
     return;
 
@@ -75,12 +131,30 @@ void loop()
     lcd.print("Work"); 
 
     work = true;
+    
+    count = 0;
+
+    cycle++;
 
     bip();
 
     }else{
 
-      lcd.print("NO"); 
+      count++;
+
+      lcd.setCursor(0,0);
+      lcd.print("~ Pomodoro Timer"); 
+      
+      lcd.setCursor(0,1);
+
+      if(count % 2 == 1){
+      
+        lcd.print("Wait start >");
+      
+      }else{
+
+        lcd.print("Wait start <");
+      } 
     }    
   
   lastState = currentState;
